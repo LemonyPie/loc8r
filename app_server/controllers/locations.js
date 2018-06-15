794a718a1934995370e8ce287a1b31ca6868c6e8
Artemee Senin <artemee.senin@gmail.com>
2018-06-15 23:22:31 +0300

    basic routing

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,9 @@
+module.exports.homeList = function(req, res){
+  res.render('index', { title: 'Home' });
+}
+module.exports.locationInfo = function(req, res){
+  res.render('index', { title: 'Location Info' });
+}
+module.exports.addReview = function(req, res){
+  res.render('index', { title: 'Add review' });
+}
