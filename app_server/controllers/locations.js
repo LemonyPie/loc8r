86f39173d51e037cbc07e87e4d45fdff9ccd7a21
Artemee Senin <artemee.senin@gmail.com>
2018-06-19 00:09:54 +0300

    homepage controller passes data to viiews

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,31 @@
 module.exports.homeList = function(req, res){
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { 
+    title: 'Loc8r - find places to work with wi-fi near you!',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find nice places to work with wi-fi near you!'
+    },
+    locations: [{
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium Wi-Fi'],
+      distance: '100m'
+    },
+    {
+      name: 'Timespace',
+      address: '130 High Street, Reading, RG6 1PS',
+      rating: 5,
+      facilities: ['Hot drinks', 'Food', 'Premium Wi-Fi', 'Lectures', 'Free snacks'],
+      distance: '200m'
+    },{
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 4,
+      facilities: ['Hot drinks', 'Food'],
+      distance: '350m'
+    }]
+  });
 }
 module.exports.locationInfo = function(req, res){
   res.render('location-info', { title: 'Location Info' });
