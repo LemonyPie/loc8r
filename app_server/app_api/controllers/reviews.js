b869374af3ccf777f4a60918553f3c08d3753c90
Artemee Senin <artemee.senin@gmail.com>
2018-06-21 16:26:09 +0300

    reviews controller in api

---
+++ app_server/app_api/controllers/reviews.js
@@ -0,0 +1,8 @@
+const sendJsonResponse = function(res, status, content){
+  res.status(status);
+  res.json(content);
+}
+
+module.exports.locationsCreate = function(req, res){
+  sendJsonResponse(res, 200, { "status" : "success" });
+}
